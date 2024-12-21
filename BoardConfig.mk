65bb4439438ca68c7da529c599fb55263069bc63
DarkKiller28 <oyunbaskani7728@gmail.com>
2024-12-21 19:52:29 +0300

    Minor fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,11 +100,8 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
  HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/compatibility_matrix.xml  \
-  
      hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-   
      vendor/lineage/config/device_framework_matrix.xml \
-    
 hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 
 
